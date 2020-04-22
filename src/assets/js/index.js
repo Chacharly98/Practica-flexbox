c5c2d6de2da4612e338ba6b400fe6f0c2da55561
Charly <ca6326287@gmail.com>
2020-04-22 14:06:04 -0500

    proyecto

---
+++ src/assets/js/index.js
@@ -0,0 +1,23 @@
+
+
+        function sumar() {
+
+
+          var num1 = queHoraes()
+          var num2 = 8
+
+          suma = parseInt(num1) + num2
+
+          alert("la hora en India es" +suma)
+
+      }
+
+
+      function queHoraes() {
+
+
+          let nombre = prompt("Qué hora es en México?")
+          console.log(nombre)
+
+          return nombre
+      }
