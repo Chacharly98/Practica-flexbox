2244a85dd3c2fa8934a6e3872a58976cb0d4d7bf
Charly <ca6326287@gmail.com>
2020-05-15 00:53:04 -0500

    corrección

--- my-proyecto/src/App.vue
+++ my-proyecto/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="w-full min-h-screen  flex px-6 t">
     <div class="w-1/2 min-h-screen p-4 ">
-        <h1 class="italic font-serif text-5xl text-center text-purple-900 font-bold">Carrito de compras</h1>
+        <h1 class="italic font-serif text-5xl text-center text-purple-900 font-bold">Carrito de compras en Vue</h1>
         <input v-model="name" placeholder="producto" class="bg-purple-200 font-bold rounded-l-lg px-2 py-2" />
         <input v-model="price" class="bg-purple-200 font-bold rounded-r-lg px-2 py-2" />
       <span v-if="'' != name && price > 0" class="inline-flex rounded-md shadow-sm mt-6">
